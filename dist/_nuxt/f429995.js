b4972abf80ba420666c15e0939d721fa21875635
Sebastian Drzewicki <ddosdor@gmail.com>
2021-07-05 23:51:42 +0200

    (none): Again... remove dist folder :/

--- dist/_nuxt/f429995.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{327:function(n,e,t){"use strict";t.r(e);var c=t(32),l=Object(c.b)({setup:function(){}}),o=t(31),component=Object(o.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  About page\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
