64569933d61e5b12dbfbecd36ccee63c62b97d99
ddosdor <ddosdor@gmail.com>
2021-07-19 15:12:20 +0200

    (fix): Typo in projects content

--- composables/useProjects.ts
+++ composables/useProjects.ts
@@ -42,7 +42,7 @@ export const useProjects = (): UseProjectsComposable => {
       isLoadingRecentProjects.value = true;
       recentProjects.value = await $content('projects')
         .only(['title', 'npm', 'github', 'description'])
-        .sortBy('prority', 'desc')
+        .sortBy('priority', 'asc')
         .limit(RECENT_PROJECTS_LIST_LIMIT)
         .fetch<ProjectPostType>();
       isLoadingRecentProjects.value = true;
