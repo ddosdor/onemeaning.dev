bc5eff0a973c4dc463dfd671ab99cd2451337716
ddosdor <ddosdor@gmail.com>
2021-08-02 15:35:59 +0200

    (fix): Change left padding again on about me content

--- components/About/AboutMeContent.vue
+++ components/About/AboutMeContent.vue
@@ -5,7 +5,7 @@
              "
   >
     <div class="flex-shrink-0
-                lg:mt-0 lg:ml-auto lg:pl-16
+                lg:mt-0 lg:ml-auto lg:pl-12
                "
     >
       <SharedUiImage :src="require('~/assets/images/me.png')"
