4f3895f54873e55341f0d45bcfd10f2944e26e04
ddosdor <ddosdor@gmail.com>
2021-08-02 21:10:27 +0200

    (none): Remove animiation from tailwindcss config

--- components/About/AboutMeContent.vue
+++ components/About/AboutMeContent.vue
@@ -21,7 +21,7 @@
       />
     </div>
     <div class="space-y-3">
-      <p class="animate-fade-in-down-5">
+      <p>
         I've been working as a <b>Software Developer</b> (specifically with
         <RoughNotation :is-show="shouldAnimateRoughNotation"
                        type="underline"
@@ -48,7 +48,7 @@
         </RoughNotation>
         whenever possible.
       </p>
-      <p class="animate-fade-in-down-7">
+      <p>
         Over the past years, I've built from scratch and managed a development
         team of several people, with full infrastructure administration, CD/CD processes
         based on Gitlab and Docker, and a technology stack based on
@@ -61,10 +61,10 @@
         ,
         Typescript and Postrgesql.
       </p>
-      <p class="animate-fade-in-down-9">
+      <p>
         Now I am looking for new challenges and work with people for whom programming is a passion
       </p>
-      <p class="animate-fade-in-down-11">
+      <p>
         Let's hang out on <a class="font-bold app-link"
                              :href="linkedin"
                              tagret="__blank"
