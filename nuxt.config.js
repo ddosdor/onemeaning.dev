989df2b911062eec5a9fb2a33096d69830015420
ddosdor <ddosdor@gmail.com>
2021-08-25 13:25:51 +0200

    (none): Add meta content desctiption

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,20 @@ export default {
     base: '/',
   },
   head: {
-    title: 'onemeaning-dev',
+    title: 'onemeaning.dev - Sebastian personal blog',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description', name: 'description', content: `
+        Hi there, I'm Sebastian and fod about couple of years I work as a Software Developer (specifically with Javascript).
+        This is my personal blog where I share what I enjoy, 
+        experiment with new tools and keep notes on what I had already learned.
+      `,
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
