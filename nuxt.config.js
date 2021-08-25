948d5205d5f4c6bb3d3b3537e8ec6fd7b7890106
ddosdor <ddosdor@gmail.com>
2021-08-25 12:29:31 +0200

    (fix): Display correct fonts on production

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,18 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'crossorigin' },
+      {
+        rel: 'preload',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        media: 'print',
+        onload: 'this.onload=null;this.removeAttribute(\'media\');',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&display=swap',
+      },
     ],
   },
   css: [
